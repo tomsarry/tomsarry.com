82294753ebe77da686d539646dab96e28f8d147a
Tom <tom.sarry@mail.mcgill.ca>
2020-12-23 21:28:21 -0500

    Add animation name

--- src/components/Redirects.jsx
+++ src/components/Redirects.jsx
@@ -7,8 +7,16 @@ const Redirects = () => {
     const { t } = useTranslation()
     return ( 
         <div className="landing-main">
-            <h1 className="name">tomsarry</h1>
-            {/* <code>Student at McGill University in CS</code> */}
+            <h1 className="name">
+                <span className="letter1">t</span>
+                <span className="letter2">o</span>
+                <span className="letter3">m</span>
+                <span className="letter4">s</span>
+                <span className="letter5">a</span>
+                <span className="letter6">r</span>
+                <span className="letter7">r</span>
+                <span className="letter8">y</span>
+            </h1>
         
             <div className="table-links">
                 <a href="#techs" className="redirect">
